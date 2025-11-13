e1e4ec7ed732f5b1a13245e1b98f1abf216e51c9
Bysaedo <by.saedo@gmail.com>
2025-11-13 01:17:31 -0600

    Halfway through module 2

--- ProjectModule1/BookList/BookListAPI/urls.py
+++ ProjectModule1/BookList/BookListAPI/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 
 urlpatterns = [
-  path('books/', views.books),]
+  #path('books/', views.books),
+  #path('books/',views.BookList.as_view()),
+  #path('books/<int:pk>', views.SingleBook.as_view()),
+  path('books/', views.BookView.as_view()),
+  path('books/<int:pk>', views.BookDetailView.as_view(), name='SingleBook')
+  ]
--- ProjectModule1/BookList/BookListAPI/views.py
+++ ProjectModule1/BookList/BookListAPI/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.db import IntegrityError
-from django.http import JsonResponse
+#from django.http import HttpResponse
+#from django.db import IntegrityError
+#from django.http import JsonResponse
 from .models import Book
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import BookSerializer
+from rest_framework import generics
 
+@api_view (['GET', 'POST'])
+def books(request):
+    return Response('list of the books', status=status.HTTP_200_OK)
+
+"""class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message":"List of books by"}, status.HTTP_200_OK)
+        return Response({"message":"List of books"}, status.HTTP_200_OK) 
+    def post(self, request):
+        return Response({"title":request.data.get('title')}, status.HTTP_201_CREATED)
+    
+class SingleBook(APIView):
+    def get(self, request, pk):
+        return Response({"message":"single book with id" + str(pk)}, status.HTTP_200_OK)
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status.HTTP_200_OK)"""
+    
+class BookView(generics.ListCreateAPIView):
+    queryset=Book.objects.all()
+    serializer_class=BookSerializer
+class BookDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
+"""
 @csrf_exempt
 def books(request):
     if request.method =='GET':
@@ -21,4 +55,5 @@ def books(request):
         except IntegrityError:
             return JsonResponse({'error': 'true', 'message': 'required field missing'}, status =400)
         
-        return JsonResponse(model_to_dict(book), status=201)
+        return Response('list of the books', status=status.HTTP_200_OK)
+    """
